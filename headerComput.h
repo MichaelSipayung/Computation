2620fd7c524c8cf8dff3205256270ce7bf36595c
Michael Sipayung <michaelsipayung123@gmail.com>
2021-08-16 07:26:42 +0700

    adding qsort adding gsl to CMakeLists.txt

--- headerComput.h
+++ headerComput.h
@@ -6,6 +6,9 @@
 #define COMPUTATION_HEADERCOMPUT_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <gsl/gsl_sf_bessel.h>
 #define OUT 0
 #define IN 1
 #define ERR -1
@@ -19,6 +22,19 @@ void copy(char[],char[]);
 void testCopy();
 int getExline();
 void copyEx();
-int max;
-
+int binarysearch(int,int[],int);
+void shellsort(int[],int);
+void reverseStr(char[]);
+int getNLine(char[],int);
+int strindex(char [],char[]);
+double atofN(char[]);
+void testBessel();
+//put here for external variable
+#define MAXVAL 100
+int sp=0;
+double val[MAXVAL];
+double pop();
+void push(double);
+void swap(int[],int ,int);
+void qsorts(int [],int,int);
 #endif //COMPUTATION_HEADERCOMPUT_H
--- main.c
+++ main.c
@@ -8,6 +8,7 @@ int main() {
     printf("power of (3,2) is \t: %d\n", power(3,2));
     //printf("Test function test copy .... ");
     //testCopy();
+    testBessel();
     return 0;
 }
 int countLine(){
@@ -112,12 +113,145 @@ int getExline(){
     int len;
     extern char line[];
     //all algorithm are equals
+    return 0;
 }
 void copyEx(){
     int i ;
     extern char line[],longest[];
     //all algorithm aare equal as before  ....
 }
+int binarysearch(int x,int v[],int n){
+    int low,high,mid;
+    low =0;
+    high = n-1;
+    while (low<=high){
+        mid=(low+high)/2;
+        if(x<v[mid]){ //change the interval
+            high = mid+1;
+        }
+        else if(x>v[mid]){ //change the interval
+            low = mid+1;
+        }
+        else{ //found match
+            return mid;
+        }
+    }
+    return -1;
+}
+void shellsort(int v[],int n){
+    int gap,i,j,temp;
+    for(gap=n/2;gap>0;gap/=2){
+        for(i=gap;i<n;i++){
+            for(j=i-gap;j>=0 &&v[j]>v[j+gap];j-=gap){ //evaluate the element ... (compare related element)
+                temp=v[j];
+                v[j]=v[j+gap];
+                v[j+gap]=temp;
+            }
+        }
+    }
+}
+void reverseStr(char s[]){
+    int c,i,j;
+    for(i=0,j= strlen(s)-1;i<j;i++,j--){
+        c=s[i];
+        s[i]=s[j];
+        s[j]=c;
+    }
+}
+int getNLine(char s[],int lim ){
+    int c,i;
+    i=0;
+    while(--lim>0 && (c=getchar())!='*' && c!='\n'){
+        s[i++]=c;
+    }
+    if(c=='\n'){
+        s[i++]=c;
+    }
+    s[i]=='\0';
+    return i;
+}
+int strindex(char s[],char t[]){
+    int i,j,k;
+    for(i=0;s[i]!='\0';i++){
+        for(j=i,k=0;t[k]!='\0' &&s[j]==t[k];j++,k++){
+            //.. not execution
+        }
+        if(k>0 && t[k]  == '\0'){
+            return i;
+        }
+    }
+    return -1;
+}
+double atofN(char s[]){
+    double val,power;
+    int i,sign;
+    for(i=0; isspace(s[i]);i++);  //stop when meet a space
+    sign  = (s[i] =='-'?-1:1);
+    if(s[i]=='+'||s[i]=='-'){
+        i++;
+    }
+    for(val=0.0; isdigit(s[i]);i++){
+        val=10.0*val+(s[i]-'0');
+    }
+    if(s[i]=='.'){
+        i++;
+    }
+    for(power=1.0; isdigit(s[i]);i++){
+        val=10.0*val+(s[i]-'0');
+        power*=10;
+    }
+    return sign*val/power;
+}
+
+void testBessel(){
+    printf("Test bessel function .... \n");
+    double  x=5.0;
+    double y= gsl_sf_bessel_j0(x);
+    printf("Bessel function result \t: ");
+    printf("j0(%g) = %.18e\n",x,y);
+}
+double pop(){
+    if(sp>0){
+        return val[--sp];
+    }
+    else{
+        printf("Sorry stack is still empty .... \n");
+        return 0.0;
+    }
+}
+
+void push(double f){
+    if(sp<MAXVAL){
+        val[sp++] = f;
+    }
+    else{
+        printf("sorry stack is full , can\'t push ..... \n");
+    }
+}
+void swap(int v[],int i ,int j){
+    int temp;
+    temp=v[i];
+    v[i]=v[j];
+    v[j]=temp;
+}
+void qsorts(int v[],int left,int right){
+    int i,last;
+    if(left>=right){
+        return;//do nothing if an array contain fewer than two
+    }
+    swap(v,left,(left+right)/2);
+    last=left;
+    for(i=left+1;i<=right;i++){ //partition
+        if(v[i]<v[left]){
+            swap(v,++last,i);
+        }
+    }
+    swap(v,left,last);
+    qsorts(v,left,last-1);
+    qsorts(v,last+1,right);
+}
+
+
 
 
 
